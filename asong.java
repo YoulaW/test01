210a8f2067a9d3c3fdf206c8f934e5d437dc07c0
Youla <youla_w@outlook.com>
2018-09-06 20:05:28 +0800

    烟巴er 所作

---
+++ asong.java
@@ -0,0 +1,8 @@
+public class Hello{
+
+        public static void main (String[] args){
+	System.out.println("Hello 纸短情长");
+	System.out.println("Love ASong");
+	System.out.println("我的故事都是关于你啊");
+        }
+}
