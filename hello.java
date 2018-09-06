c127cf9b8a8aa178a6c8ca7a5f0fa0ee46dfd329
Youla <youla_w@outlook.com>
2018-09-06 18:37:39 +0800

    add Chinese print

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class Hello{
 
         public static void main (String[] args){
 	System.out.println("Hello Git");
+	System.out.println("Love Git");
+	System.out.println("¹þ¹þ¹þ");
         }
 }
