418451b755a69023d7db9a2ceb1434498c875993
Youla <youla_w@outlook.com>
2018-09-06 20:13:54 +0800

    一部感人的电视剧

---
+++ 1988.java
@@ -0,0 +1,8 @@
+public class Hello{
+
+        public static void main (String[] args){
+	System.out.println("Hello 1988");
+	System.out.println("A Road name QingMenDong,there are 5 famliies.The story is about famliy,friend,love.");
+	
+        }
+}
